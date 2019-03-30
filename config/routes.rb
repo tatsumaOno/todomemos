158e2cfbf0a2bbd9acec5b301c45691497903db2
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-03-30 14:48:03 +0900

    create cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :list, only: [:new,:create,:edit,:update,:destroy]
+  resources :list, only: [:new,:create,:edit,:update,:destroy] do
+    resources :card,except: [:index]
+  end
   resources :top,only: [:index]
   root 'top#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_28_134636) do
+ActiveRecord::Schema.define(version: 2019_03_29_064747) do
+
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "memo"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -33,5 +42,6 @@ ActiveRecord::Schema.define(version: 2019_03_28_134636) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "lists"
   add_foreign_key "lists", "users"
 end
