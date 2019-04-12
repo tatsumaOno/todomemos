2cde7cd974e093fd39656798e2dd4cc65b0188ad
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-04-13 00:24:49 +0900

    create user model test

--- Gemfile
+++ Gemfile
@@ -45,7 +45,6 @@ gem "chartkick"
 gem 'rspec-rails'
 gem 'factory_bot_rails'
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'devise'
   gem 'pry-rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+    it 'valid all column' do
+      user = build(:user)
+      expect(user).to be_valid
+    end
     it 'invalid without name' do
-      user = User.new(name: "",email: "hoge@gmail.com",password: '12345678910',password_confirmation: "12345678910")
+      user = build(:user, name: "")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
 
     it 'invalid without email' do
-      user = User.new(name: "tarou",email: "",password: '12345678910',password_confirmation: "12345678910")
+      user = build(:user,email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
+    end
+    it 'invalid duplicate email address' do
+      user = create(:user)
+      another_user = build(:user,email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
   end
 end
