92ae3f304e115c8cc2afb4d22a2c391b29eb8513
tatsumaOno <onotatsuma@onodarumanoMacBook-puro.local>
2019-04-13 15:03:35 +0900

    spec top#index

--- spec/controllers/list_controller_spec.rb
+++ spec/controllers/list_controller_spec.rb
@@ -22,3 +22,18 @@ describe ListController do
     end
   end
 end
+
+describe TopController do
+  describe 'GET #index' do
+    it 'populates an array of list ordered by created_at DESC' do
+        list = create_list(:list, 3)
+        get :index
+        expect(assigns(:list)).to match(list)
+    end
+
+    it 'renders the :index template' do
+
+    end
+  end
+
+end
